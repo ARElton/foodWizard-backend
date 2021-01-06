a0afa63425915a71c5e8734672005ff3754375de
ARElton <ameliareneeelton@gmail.com>
2021-01-06 11:55:08 -0500

    update db with favorites

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_04_224623) do
+ActiveRecord::Schema.define(version: 2021_01_06_164856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_04_224623) do
     t.integer "star_rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "favorites"
   end
 
   create_table "recipes", force: :cascade do |t|
