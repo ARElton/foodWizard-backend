5fd263fd20153fd73f7fc44e4a796fb501538eb9
ARElton <ameliareneeelton@gmail.com>
2021-01-06 21:57:28 -0500

    login feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 namespace :api do
   namespace :v1 do
-    resources :recipes, only: [:show, :index]
+    get "/recipes", to: "recipes#index"
+    get "/recipes/:id", to: "recipes#show"
+
     post "/recipes", to: "recipes#create"
+    post "/login", to: "users#login"
   end
 end
 
